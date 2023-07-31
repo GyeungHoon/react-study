bae070d3f78316c228a5c7e4da6efb9379861fe8
GyeungHoon <ghp0415@naver.com>
2023-07-31 18:12:09 +0900

    버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
 
-  let [글제목,b] = useState('남자 코드 추천');
+  let [글제목,b] = useState(['남자 코트 추천','여자 코트 추천','유니섹스 코트 추천']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -11,7 +12,17 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
